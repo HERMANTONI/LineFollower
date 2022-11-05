4980700dd3c63f5b18102bdc10250d376f962264
HERMANTONI <82546034+HERMANTONI@users.noreply.github.com>
2022-11-05 16:16:22 +0700

    Create ProgramUtama.ino

---
+++ ProgramUtama.ino
@@ -0,0 +1,19 @@
+/*
+ * Developer  : Hermantoni
+ * Email      : hermantoni0510@gmail.com
+ * Board      : Arduino nano
+ * Kategori   : LineFollower
+ * Bagian     : ProgramUtama
+ * 
+ */
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int Desimal = Sensor2Biner();
+  Serial.print(Desimal); Serial.print(" ");
+  Serial.print(Desimal, BIN); Serial.println(" ");
+  delay(500);
+}
