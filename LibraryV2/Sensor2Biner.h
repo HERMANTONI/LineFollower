25019a7224fa74c9d159bf850abe0463be0676d2
HERMANTONI <82546034+HERMANTONI@users.noreply.github.com>
2023-10-31 20:52:52 +0700

    Update Sensor2Biner.h

--- LibraryV2/Sensor2Biner.h
+++ LibraryV2/Sensor2Biner.h
@@ -1,1 +1,31 @@
-int a = 0;
+/*
+ * Developer  : Hermantoni
+ * Email      : hermantoni0510@gmail.com
+ * Board      : Arduino nano
+ * Kategori   : LineFollower
+ * Bagian     : Sensor2Biner
+ * 
+ */
+/*
+!!!Copy code to root program!!!
+int Sensor_eeprom[]={200,210,210,210,120};
+int JumlahSensor = 5;
+*/
+
+int Sensor2Biner(){
+  int Pangkat[] = {1, 2, 4, 8, 16, 32, 64, 128};
+  int Sensor[] = {0,0,0,0,0};
+  int TempSensor = 0;
+  for (int a=0; a<JumlahSensor; a++){
+    Sensor[a] = analogRead(a);
+    Serial.println(Sensor[a]);
+    // delay(100);
+  }
+  for (int a=0; a<JumlahSensor; a++){
+    if (Sensor[a] > Sensor_eeprom[a]){
+      TempSensor = TempSensor | Pangkat[a];
+    }
+  }
+  Serial.println(TempSensor,BIN);
+  return TempSensor;
+}
