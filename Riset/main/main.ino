fb246acf226986f3773c006d2bb853fc794ac517
HERMANTONI <82546034+HERMANTONI@users.noreply.github.com>
2023-11-08 22:26:43 +0700

    Update main.ino

--- Riset/main/main.ino
+++ Riset/main/main.ino
@@ -2,9 +2,10 @@
 
 int Sensor_eeprom[]={200,210,210,210,120};
 int JumlahSensor = 5;
+int StopLoop = 0;
 
 void setup() {
-  Serial.begin(9600);
+ Serial.begin(9600);
   pinMode(2,1); pinMode(3,1); pinMode(4,1); pinMode(5,1);
   pinMode(8,INPUT_PULLUP); pinMode(13,1);
   if(digitalRead(8) == 0) setSensor(); 
@@ -13,7 +14,7 @@ void setup() {
     Sensor_eeprom[a] = EEPROM.read(a) * 4;
     Serial.print(Sensor_eeprom[a]); Serial.print(" ");
   }
-  Serial.println("Ready to Go");
+  Serial.println("Ready to Go"); 
 }
 
 void loop() {
@@ -42,59 +43,139 @@ int Sensor2Biner(){
 }
 
 int Biner2Motor(int Biner){
-  if (Biner == 0b11111) Motor(1,1);
-  else if (Biner == 0b00001) Motor(2,0);
-  else if (Biner == 0b00010) Motor(2,1);
-  else if (Biner == 0b00100) Motor(3,3);
-  else if (Biner == 0b01000) Motor(1,2);
-  else if (Biner == 0b10000) Motor(0,2);
+  if (StopLoop < 400){
+    if (Biner == 0b11111){
+      if (StopLoop < 3000) StopLoop +=1;
+    }
+    else if (Biner == 0b00000) {
+      if (StopLoop < 3000) StopLoop +=1;
+    }
 
-  else if (Biner == 0b00011) Motor(2,1);
-  else if (Biner == 0b00110) Motor(3,2);
-  else if (Biner == 0b01100) Motor(2,3);
-  else if (Biner == 0b11000) Motor(1,2);
-  
-  else if (Biner == 0b00111) Motor(1,2);
-  else if (Biner == 0b01110) Motor(3,3);
-  else if (Biner == 0b11100) Motor(2,1);
-  
-  else if (Biner == 0b11110) Motor(2,0);
-  else if (Biner == 0b11101) Motor(2,1);
-  else if (Biner == 0b11011) Motor(3,3);
-  else if (Biner == 0b10111) Motor(1,2);
-  else if (Biner == 0b01111) Motor(0,2);
+    else if (Biner == 0b00001) Motor(2,-1);
+    else if (Biner == 0b00010) Motor(2,0);
+    else if (Biner == 0b00100) Motor(3,3);
+    else if (Biner == 0b01000) Motor(0,2);
+    else if (Biner == 0b10000) Motor(-1,2);
 
-  else if (Biner == 0b11100) Motor(2,1);
-  else if (Biner == 0b11001) Motor(3,2);
-  else if (Biner == 0b10011) Motor(2,3);
-  else if (Biner == 0b00111) Motor(1,2);
+    else if (Biner == 0b00011) Motor(2,0);
+    else if (Biner == 0b00110) Motor(3,1);
+    else if (Biner == 0b01100) Motor(1,3);
+    else if (Biner == 0b11000) Motor(0,2);
+    
+    else if (Biner == 0b00111) Motor(0,2);
+    else if (Biner == 0b01110) Motor(3,3);
+    else if (Biner == 0b11100) Motor(2,0);
+    
+    else if (Biner == 0b11110) Motor(2,-1);
+    else if (Biner == 0b11101) Motor(2,0);
+    else if (Biner == 0b11011) Motor(3,3);
+    else if (Biner == 0b10111) Motor(0,2);
+    else if (Biner == 0b01111) Motor(-1,2);
 
-  else if (Biner == 0b00101) Motor(2,1);
-  else if (Biner == 0b01010) Motor(3,3);
-  else if (Biner == 0b10100) Motor(1,2);
-  else if (Biner == 0b10101) Motor(3,3);
+    else if (Biner == 0b11100) Motor(2,0);
+    else if (Biner == 0b11001) Motor(3,1);
+    else if (Biner == 0b10011) Motor(0,3);
+    else if (Biner == 0b00111) Motor(0,2);
 
-  else if (Biner == 0b01101) Motor(2,3);
-  else if (Biner == 0b11010) Motor(2,3);
-  else if (Biner == 0b10110) Motor(3,2);
-  else if (Biner == 0b01011) Motor(3,2);
+    else if (Biner == 0b00101) Motor(2,0);
+    else if (Biner == 0b01010) Motor(3,3);
+    else if (Biner == 0b10100) Motor(0,2);
+    else if (Biner == 0b10101) Motor(3,3);
 
-  else if (Biner == 0b00000) Motor(1,1);
+    else if (Biner == 0b01101) Motor(1,3);
+    else if (Biner == 0b11010) Motor(3,1);
+    else if (Biner == 0b10110) Motor(3,1);
+    else if (Biner == 0b01011) Motor(1,3);
+  }
+  else {
+    Motor(0,0);
+  }
   return 0;
 }
 
 int Motor(int Kiri, int Kanan){
-  int SpeedReff = 255; //range 155-255
-  int KN1 = 5, KN0 = 4, KR1 = 3, KR0 = 2;
-  int SumKR = SpeedReff - (Kiri*50);
-  int SumKN = SpeedReff - (Kanan*50);
-  analogWrite(KR1, SumKR);
-  analogWrite(KN1, SumKN);
-  digitalWrite(KR0, 1);
-  digitalWrite(KN0, 1);
-
+  StopLoop = 0;
+  MotorV2(Kiri, Kanan);
+  // int KN1 = 5, KN0 = 4, KR1 = 3, KR0 = 2;
+  // if (Kiri != 0 and Kanan !=0){
+  //   int SpeedReff = 155; //range 155-255
+  //   int SumKR = SpeedReff - (Kiri*50);
+  //   int SumKN = SpeedReff - (Kanan*50);
+  //   if (Kanan == 0){
+  //     analogWrite(KR1, SumKR);
+  //     analogWrite(KN1, 120);
+  //     digitalWrite(KR0, 1);
+  //     digitalWrite(KN0, 0);
+  //   }
+  //   else if (Kiri == 0){
+  //     analogWrite(KR1, 120);
+  //     analogWrite(KN1, SumKN);
+  //     digitalWrite(KR0, 0);
+  //     digitalWrite(KN0, 1);
+  //   }
+  //   else {
+  //     analogWrite(KR1, SumKR);
+  //     analogWrite(KN1, SumKN);
+  //     digitalWrite(KR0, 1);
+  //     digitalWrite(KN0, 1);
+  //   }
+  // }
+  // else {
+  //   digitalWrite(KR1, 0);
+  //   digitalWrite(KN1, 0);
+  //   digitalWrite(KR0, 0);
+  //   digitalWrite(KN0, 0);
+  // }
   // Serial.print(SumKR); Serial.print(" "); Serial.print(SumKN); Serial.print(" ");
   return 0;
+}
+
+int MotorV2(int Kiri, int Kanan){
+  int SpeedReff = 155; //range 155-255
+  int KN1 = 5, KN0 = 4, KR1 = 3, KR0 = 2;
+  int SumKR, SumKN;
+  if (Kanan == 0 and Kiri == 0){
+    digitalWrite(KR1, 1);
+    digitalWrite(KN1, 1);
+    digitalWrite(KR0, 1);
+    digitalWrite(KN0, 1);
+  }
+  else if (Kanan == Kiri){
+    digitalWrite(KR1, 0);
+    digitalWrite(KN1, 0);
+    digitalWrite(KR0, 1);
+    digitalWrite(KN0, 1);
+  }
+  else if (Kanan > Kiri){
+    if (Kiri < 0){
+      SumKR = Kiri*-100;
+      digitalWrite(KR0, 0);
+      analogWrite(KR1, SumKR);
+    }
+    else {
+      SumKR = SpeedReff - (Kiri*50);
+      digitalWrite(KR0, 1);
+      analogWrite(KR1, SumKR);
+    }
+    SumKN = SpeedReff - (Kanan*50);
+    analogWrite(KN1, SumKN);
+    digitalWrite(KN0, 1);
+  }
+  else if (Kanan < Kiri){
+    if (Kanan < 0){
+      SumKN = Kanan*-100;
+      digitalWrite(KN0, 0);
+      analogWrite(KN1, SumKN);
+    }
+    else {
+      SumKN = SpeedReff - (Kanan*50);
+      digitalWrite(KN0, 1);
+      analogWrite(KN1, SumKN);
+    }
+    SumKR = SpeedReff - (Kiri*50);
+    analogWrite(KR1, SumKR);
+    digitalWrite(KR0, 1);
+  }
 }
 
 int setSensor(){
@@ -162,7 +243,7 @@ int setSensor(){
     Serial.println();
     for (int a=0; a<5; a++){
       SensorM[a] = SensorH[a] - SensorL[a];
-      SensorH[a] = SensorM[a] / 2;
+      SensorH[a] = SensorM[a] / 3;
       Serial.print(SensorH[a]); Serial.print(" ");
       SensorM[a] = SensorH[a] + SensorL[a];
       EEPROM.write(a, SensorM[a] / 4);
